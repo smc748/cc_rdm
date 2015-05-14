5138ac8bd552b2a56b4f4bf40f6edaf190cfb605
smc748 <sean.cavanaugh@usask.ca>
2015-05-14 10:47:19 -0600

    Enable distutils functionality for package building.

--- setup.py
+++ setup.py
@@ -1,6 +1,8 @@
-from setuptools import setup
+from distutils.core import setup
 
 setup(	name="cc_rdm",
 		version="0.1",
 		description="Compute Canada RDM Pilot API",
-		packages=["cc_rdm"]
+		packages=["cc_rdm"],
+		author='Sean Cavanaugh',
+		author_email='sean.cavanaugh@usask.ca')
